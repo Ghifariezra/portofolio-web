2092991f1ee3ecaaeab0fbc9775de1984449d2c1
Owner <ghifariezraramadhan@gmail.com>
2025-05-07 12:21:30 +0700

    initial commit

--- src/components/section/Project.jsx
+++ src/components/section/Project.jsx
@@ -3,7 +3,7 @@ import ProjectComponents from '@section/content/Project'
 
 function Project() {
     return (
-        <section id='project' className='mx-auto select-none bg-sky-100 py-4'>
+        <section id='project' className='mx-auto select-none bg-sky-100 py-6 overflow-hidden'>
             <ProjectComponents />
         </section>
     )
