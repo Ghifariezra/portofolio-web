86d13278d23c0ef4261c1ac1b84cf2d0d4d3d883
Ezra <127650295+Ghifariezra@users.noreply.github.com>
2025-05-07 11:39:35 +0700

    Delete dist directory

--- dist/assets/Certificate-CeDRTDvp.js
+++
@@ -1,1 +0,0 @@
-import{j as t,r as e}from"./index-CVph5rec.js";function o(){return t.jsx(e.StrictMode,{children:t.jsx("h1",{className:"text-3xl font-bold underline",children:"Hello world!"})})}export{o as default};
