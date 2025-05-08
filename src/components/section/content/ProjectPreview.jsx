87b30d322f8435ba8acf54744395acf0936f797a
Owner <ghifariezraramadhan@gmail.com>
2025-05-08 21:18:39 +0700

    update

--- src/components/section/content/ProjectPreview.jsx
+++ src/components/section/content/ProjectPreview.jsx
@@ -42,7 +42,7 @@ function ProjectPreview() {
     return (
         <div className='project-content'>
             <div className='head-project' data-aos="fade-up">
-                <h1 className='text-project'>Project Preview</h1>
+                <h1 className='text-project'>Projects</h1>
             </div>
             <div className='wrapper-card'>
                 {projects.map((project, index) => (
