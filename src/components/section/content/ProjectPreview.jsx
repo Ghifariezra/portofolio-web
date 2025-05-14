dbf5099b2463095b07639f6edbad4097f704e3ab
Owner <ghifariezraramadhan@gmail.com>
2025-05-15 06:06:28 +0700

    add dropdown mobile

--- src/components/section/content/ProjectPreview.jsx
+++ src/components/section/content/ProjectPreview.jsx
@@ -1,4 +1,6 @@
 import { useState, useRef, useEffect } from 'react'
+import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
+import ArrowDropUpRoundedIcon from '@mui/icons-material/ArrowDropUpRounded';
 import ProjectCardPreview from '@section/content/ProjectCardsPreview'
 import { fetchProjects } from '@services/MyAPI'
 
@@ -7,6 +9,7 @@ function ProjectPreview() {
   const [seeMore, setSeeMore] = useState(false)
   const [active, setActive] = useState('All')
   const [projects, setProjectData] = useState([])
+  const [dropdown, setDropdown] = useState(false)
 
   // Filter project
   const filterProject = active === 'All' ? projects : projects.filter((item) => item.category === active)
@@ -17,6 +20,12 @@ function ProjectPreview() {
   const handleSeeMore = () => {
     setSeeMore(seeMore => {
       return !seeMore
+    })
+  }
+
+  const handleDropdown = () => {
+    setDropdown(dropdown => {
+      return !dropdown
     })
   }
 
@@ -36,7 +45,7 @@ function ProjectPreview() {
       console.log(error)
     }
   }
-  
+
   useEffect(() => {
     getProjects()
   }, [])
@@ -52,6 +61,48 @@ function ProjectPreview() {
       <div className="head-project" data-aos="fade-up">
         <h1 className="text-project">Projects</h1>
       </div>
+
+      {/* Mobile Dropdown */}
+      {/* <div className="sm:hidden px-0 py-4">
+        <select
+          className="w-full px-4 py-2 rounded-xl bg-white text-sky-500 font-bold"
+          value={active}
+          onChange={(e) => setActive(e.target.value)}
+        >
+          {catalog.map((item, index) => (
+            <option key={index} value={item}>{item}</option>
+          ))}
+        </select>
+      </div> */}
+      <div className='dropdown'>
+        <div className="dropdown-btn" onClick={handleDropdown}>
+          {active} {
+            dropdown ? <ArrowDropDownRoundedIcon /> : <ArrowDropUpRoundedIcon />
+          }
+        </div>
+        {dropdown && (
+          <div className="dropdown-content">
+            {catalog.map((item, index) => (
+              <div
+                key={index}
+                onClick={() => {
+                  setActive(item)
+                  setDropdown(false)
+                }}
+                className={`dropdown-item
+                  ${active === item
+                    ? 'bg-sky-500 text-white font-bold'
+                    : 'bg-white text-sky-500 hover:bg-sky-500 hover:text-white'
+                  }`}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        )
+        }
+      </div>
+
       <div className="category-project">
         {catalog.map((item, index) => (
           <div
