b279356e8e2e40e481dbccf3806bddedbe40fe05
Owner <ghifariezraramadhan@gmail.com>
2025-05-20 08:43:47 +0700

    update cache react

--- src/services/Quotes.jsx
+++ src/services/Quotes.jsx
@@ -1,14 +1,22 @@
+import { cache } from 'react'
+
 const QuotesAPI = import.meta.env.VITE_QUOTES_API;
 
 async function fetchData() {
-    try {
-        const response = await fetch(QuotesAPI);
-        const data = await response.json();
-        return data; // PENTING: return hasilnya
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
+    cache(async () => {
+        const data = await fetch(QuotesAPI)
+            .then(res => res.json())
+            .then(data => {
+                return data
+            }).catch(error => {
+                console.error('Error fetching data:', error);
+                return null; // or handle the error as needed
+            })
+        if (!data) {
+            throw new Error('Failed to fetch data');
+        }
+        return data
+    })()
 }
 
 export default fetchData;
