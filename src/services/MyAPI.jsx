aaa3b3c42b27f5f8dd6216f0c829b4019c8f4c06
Owner <ghifariezraramadhan@gmail.com>
2025-05-20 08:41:51 +0700

    update cache react

--- src/services/MyAPI.jsx
+++ src/services/MyAPI.jsx
@@ -1,38 +1,41 @@
-const fetchProjects = async () => {
+import { cache } from 'react'
+
+const fetchProjects = cache(async () => {
     const data = await fetch(import.meta.env.VITE_API_PROJECT)
         .then(res => res.json())
         .then(data => {
             return data
         })
-
     return data
-}
-const fetchCertificate = async () => {
+})
+const fetchCertificate = cache(async () => {
     const data = await fetch(import.meta.env.VITE_API_CERTIFICATE)
         .then(res => res.json())
         .then(data => {
             return data
         })
-
     return data
-} 
-const fetchHero = async () => {
-    const data = await fetch(import.meta.env.VITE_API_HERO)
-        .then(res => res.json())
-        .then(data => {
-            return data
-        })
+})
+const fetchHero = cache(
+    async () => {
+        const data = await fetch(import.meta.env.VITE_API_HERO)
+            .then(res => res.json())
+            .then(data => {
+                return data
+            })
+        return data
+    }
+);
 
-    return data
-}
-
-const fetchQuotes = async () => {
-    const data = await fetch(import.meta.env.VITE_API_QUOTES)
-        .then(res => res.json())
-        .then(data => {
-            return data
-        })
-    return data
-}
+const fetchQuotes = cache(
+    async () => {
+        const data = await fetch(import.meta.env.VITE_API_QUOTES)
+            .then(res => res.json())
+            .then(data => {
+                return data
+            })
+        return data
+    }
+)
 
 export { fetchProjects, fetchCertificate, fetchHero, fetchQuotes }
